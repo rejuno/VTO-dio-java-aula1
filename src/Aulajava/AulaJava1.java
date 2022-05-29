14e72d4c4da2b98a7404d97f5921772ccf735985
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-05-29 02:50:43 -0300

    exercicio 1 da aula de VTO Java do Bootcamp Carrefour (DIO).

---
+++ src/Aulajava/AulaJava1.java
@@ -0,0 +1,46 @@
+package Aulajava;
+
+public class AulaJava1 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int I;
+		int _1a;/*pode criar mas não é recomendado*/
+		int $aq;/*pode criar mas não é recomendado*/
+		
+		i = 5;
+		I=10;
+		_1a=20;
+		$aq=7;
+		
+		final int j = 10;
+		int asrn2$4678_md = 10;
+		int asrn24678md;
+		
+		asrn24678md = 100;
+		asrn2$4678_md = 10;
+		
+		int quantidadeProduto = 50;
+		final int NUMERO_TENTATIVAS = 5;
+		int QUANTIDADE_OPCOES = 25;/*nao segue a recomendacao, mas pode ficar*/
+		
+		System.out.println(i);
+		System.out.println(I);
+		System.out.println(_1a);
+		System.out.println($aq);
+		
+		System.out.println(j);
+		System.out.println(asrn2$4678_md);
+		System.out.println(asrn24678md);
+		
+		System.out.println(quantidadeProduto);
+		System.out.println(NUMERO_TENTATIVAS);
+		System.out.println(QUANTIDADE_OPCOES);
+		
+		
+		
+		
+	}
+
+}
