7756c0f9873e22999a1d41a0f30d89d31485552d
Renata Novais <98842524+rejuno@users.noreply.github.com>
2022-05-29 03:23:55 -0300

    exercicio 2 VTO Java BootCamp Carrefour (Dio)

---
+++ src/Aulajava/AulaJava2.java
@@ -0,0 +1,55 @@
+package Aulajava;
+
+public class AulaJava2 {
+	public static void main(String[] args) {
+	
+	byte b1 = 10;
+	byte b2 = 20;
+	
+	short s1 = 2000;
+	short s2 = 3000;
+	
+	int i1 = 2;
+	int i2 = 3;
+	
+	long l1 = 1000000000L;
+	long l2 = 2000000000L;
+	
+	float f1 = 3.14f;
+	float f2 = 2.25f;
+	
+	double d1= 4.45d;
+	double d2 = 5.55d;
+	
+	char c1 = 'R';
+	char c2 = 'E';
+	
+	String st1 = "oi";
+	String st2 = "tchau";
+	
+	boolean bo1 = true;
+	boolean bo2 = false;
+	
+	System.out.println(b1);
+	System.out.println(b2);
+	System.out.println(s1);
+	System.out.println(s2);
+	System.out.println(i1);
+	System.out.println(i2);
+	System.out.println(l1);
+	System.out.println(l2);
+	System.out.println(f1);
+	System.out.println(f2);
+	System.out.println(d1);
+	System.out.println(d2);
+	System.out.println(c1);
+	System.out.println(c2);
+	System.out.println(st1);
+	System.out.println(st2);
+	System.out.println(bo1);
+	System.out.println(bo2);
+	
+	
+	
+}
+}
